b93b394fa70597d365bf88dfed255f68cf670000
interestingLSY <2659723130@qq.com>
2020-01-18 15:58:59 +0800

    添加页面：后台管理中 总览

--- sites/admin.py
+++ sites/admin.py
@@ -6,7 +6,20 @@ def AdminHomeRun():
 	operator = modules.GetCurrentOperator()
 	if not modules.CheckPrivilege(operator,'system_admin'):
 		return modules.RedirectBack('无此权限')
-	return render_template('admin.html')
+
+	statistic_datas = {
+		'problems_count': db.Execute('SELECT COUNT(*) FROM problems')[0]['COUNT(*)'],
+		'submissions_count': db.Execute('SELECT COUNT(*) FROM submissions')[0]['COUNT(*)'],
+		'users_count': db.Execute('SELECT COUNT(*) FROM users')[0]['COUNT(*)']
+	}
+
+	system_infos = {}
+	try:
+		with open('.git/FETCH_HEAD','r') as f:
+			system_infos['version_number'] = f.readline().split()[0]
+	except:
+		pass
+	return render_template('admin.html',statistic_datas=statistic_datas,system_infos=system_infos)
 
 def AdminRejudgeRun():
 	operator = modules.GetCurrentOperator()
